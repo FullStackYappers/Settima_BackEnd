eab8757cf98f208e26dac038830b3aeafb5d1a7c
Naren Prakash <92686830+trippletrouble@users.noreply.github.com>
2024-12-11 01:21:12 +0100

    made changes to protected

--- app/Http/Controllers/Kernel.php
+++ app/Http/Controllers/Kernel.php
@@ -59,10 +59,9 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-    ];
-
-    protected $routeMiddleware = [
-        // Other middleware...
         'is_admin' => \App\Http\Middleware\IsAdmin::class,
     ];
+
+    
+
 }
