2f6d4992d969b51c2d5b0e9040e952960db00d2b
fadhil <116274764+Fadfah@users.noreply.github.com>
2024-12-31 22:08:05 +0100

    small change

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -10,7 +10,7 @@ class MovieController extends Controller
 {
     public function index()
     {
-        $movies = Movie::all();
+        $movies = Movie::inRandomOrder() -> get();
         return response()->json($movies);
     }
 
